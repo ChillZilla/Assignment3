fd79952dfec8cf359b6d3fd73f01c5846f45f910
Chillzilla <amanda.akin@live.com>
2016-02-19 18:52:26 -0600

    committed ParsedInput.java

---
+++ ParsedInput.java
@@ -0,0 +1,33 @@
+package Assignment3;
+
+public class ParsedInput {
+	String command;
+	String category;
+	String name;
+	int price;
+	int quantity;
+	int weight;
+	String optional1;
+	String optional2;
+	
+	ParsedInput(String operation, String cat, String item, String cost, int value, int amount, int mass, String op1, String op2)
+	{
+		command = operation;
+		category = cat;
+		name = item;
+		price = value;
+		quantity = amount;
+		weight = mass;
+		optional1 = op1;
+		optional2 = op2;
+	}
+	ParsedInput()
+	{
+		command = category = name = "";
+		price = quantity = weight = 0;
+		optional1 = optional2 = "";
+	}
+	
+}
+
+
